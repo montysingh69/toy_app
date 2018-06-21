98a53c792cab61e0c3f0851faa4e3f23da8f75be
Rohit Ner <rohitner1@gmail.com>
2018-06-21 16:42:48 +0530

    add microposts and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # create large number of pages for manipulating users
+  resources :microposts
+  # create large number of pages for manipulating users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
+  root 'users#index'
 end
